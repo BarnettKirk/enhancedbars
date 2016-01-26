9306032e20252afa4917101433120bd7c5ef930d
BarnettKirk <barnett.kirk@gmail.com>
2016-01-26 14:43:23 +1100

    added points on/off option

--- enhancedbars.R
+++ enhancedbars.R
@@ -1,5 +1,5 @@
 enhancedbarsAlpha <- function(dataframe,resp,treat,meandf,sedf,colpal=palette(), ylabel=NULL, maintitle=NULL,xlabel=NULL, 
-                         yadj=-1.7,ylimit=range(resp[!is.na(resp) & is.finite(resp)]), alpha=0.7,yaxis="s",...){
+  yadj=-1.7,ylimit=range(resp[!is.na(resp) & is.finite(resp)]), alpha=0.7,yaxis="s",points=TRUE,...){
   
   plot((1:length(meandf)),meandf, ylim=ylimit, 
       cex.axis=1.2, cex=1.2, xlim=c(0.5,length(meandf)+0.5),type='n', ylab="",xaxt='n', yaxt=yaxis, xlab="",main=maintitle)
@@ -7,11 +7,12 @@ enhancedbarsAlpha <- function(dataframe,resp,treat,meandf,sedf,colpal=palette(),
   for(i in 1:length(meandf)){
     rect(i-0.3,0,i+0.3,meandf[i], col=alpha(colpal[i],alpha=alpha), ...)
   }
-  
+  if(points == TRUE){
   for(i in 1:length(meandf)){
     points(jitter(rep(i,nrow(dataframe[treat==levels(treat)[i],]))), 
            jitter(resp[treat==levels(treat)[i]], factor=0.1), bg=alpha(colpal[i],alpha=alpha), pch=21)
-  }
+    }
+  }else{}
   
   for(i in 1:length(meandf)){
     arrows(i, meandf[i], i, meandf[i]+sedf[i], 
@@ -25,7 +26,7 @@ enhancedbarsAlpha <- function(dataframe,resp,treat,meandf,sedf,colpal=palette(),
 }
 
 enhancedbarsDen <- function(dataframe,resp,treat,meandf,sedf,colpal=palette(), ylabel=NULL, maintitle=NULL,xlabel=NULL, 
-            yadj=-1.7,ylimit=range(resp[!is.na(resp) & is.finite(resp)]), density=NULL, angle=NULL, alpha=0.7,...){
+            yadj=-1.7,ylimit=range(resp[!is.na(resp) & is.finite(resp)]), density=NULL, angle=NULL, alpha=0.7,yaxis="s",points=TRUE,...){
   
   plot((1:length(meandf)),meandf, ylim=ylimit, 
        cex.axis=1.2, cex=1.2, xlim=c(0.5,length(meandf)+0.5),type='n', ylab="",xaxt='n', xlab="",main=maintitle)
@@ -33,11 +34,12 @@ enhancedbarsDen <- function(dataframe,resp,treat,meandf,sedf,colpal=palette(), y
   for(i in 1:length(meandf)){
     rect(i-0.3,0,i+0.3,meandf[i], col=colpal[i], density=density[i], angle=angle[i], ...)
   }
-  
+  if(points == TRUE){
   for(i in 1:length(meandf)){
     points(jitter(rep(i,nrow(dataframe[treat==levels(treat)[i],]))), 
            jitter(resp[treat==levels(treat)[i]], factor=0.1), bg=alpha(colpal[i],alpha=alpha), pch=21)
-  }
+      }
+    }else{}
   
   for(i in 1:length(meandf)){
     arrows(i, meandf[i], i, meandf[i]+sedf[i], 
